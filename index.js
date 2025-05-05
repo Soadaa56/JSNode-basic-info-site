06dc48bcf6ef9ffadf1bb24d18f7ca8f16e44cf6
Kevin Duffy <soadhlh@gmail.com>
2025-05-05 16:49:40 -0400

    Specific urls functional with 404 fallback
    Project parameters complete

--- index.js
+++ index.js
@@ -3,11 +3,35 @@ const fs = require("node:fs");
 
 // Local server
  const server = http.createServer((req, res) => {
-    const test = "test var";
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    let html = fs.readFileSync("./index.html", "utf-8");
-    html = html.replace("{{test}}", test);
-    res.end(html);
+    let html;
+
+    switch (req.url) {
+        case "/":
+            res.writeHead(200, { "Content-Type": "text/html" });
+            html = fs.readFileSync("./index.html", "utf-8");
+            res.end(html);
+            break;
+        case "/about":
+            res.writeHead(200, { "Content-Type": "text/html" });
+            html = fs.readFileSync("./about.html", "utf-8");
+            res.end(html);
+            break;
+        case "/contact-me":
+            res.writeHead(200, { "Content-Type": "text/html" });
+            html = fs.readFileSync("./contact-me.html", "utf-8");
+            res.end(html);
+            break;
+        case "/contactme":
+            res.writeHead(200, { "Content-Type": "text/html" });
+            html = fs.readFileSync("./contact-me.html", "utf-8");
+            res.end(html);
+            break;
+        default:
+            res.writeHead(404, { "Content-Type": "text/html" });
+            html = fs.readFileSync("./404.html", "utf-8");
+            res.end(html);
+            break;
+    }
  });
 
  server.listen(8080, () => {
