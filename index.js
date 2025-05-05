3ae54d89f672f97d6fea71d4ba3a52b4ff34aef9
Kevin Duffy <soadhlh@gmail.com>
2025-05-05 16:10:46 -0400

    Index.js to handle server requests

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('node:http');
+
+// Local server
+ const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+        data: 'Hello World!',
+    }));
+ });
+
+ server.listen(8080);
